f3ab10fb5fed79a260d2d932d3c031cf589ce7f2
Morgan Thompson <morgan.thompson222@icloud.com>
2026-06-26 17:49:53 +0100

    Fix GA4: remove send_page_view:false so initial page view fires

--- app/layout.tsx
+++ app/layout.tsx
@@ -115,7 +115,7 @@ export default function RootLayout({
                 window.dataLayer = window.dataLayer || [];
                 function gtag(){dataLayer.push(arguments);}
                 gtag('js', new Date());
-                gtag('config', '${process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID}', { send_page_view: false });
+                gtag('config', '${process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID}');
               `}
             </Script>
           </>
